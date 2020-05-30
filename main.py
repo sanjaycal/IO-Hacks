a5f324a0b531008a059cf188bf21fc7661a5427f
Sanjay Jayaram <sanjay.calgary@gmail.com>
2020-05-29 20:01:37 -0600

    made people

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pygame
 import time
 import sys
+class person():
+    __init__(self, infected, dead, immune,x,y):
+        self.infected = infected
+        self.dead = dead
+        self.immune = immune
+        self.x = x
+        self.y = y
+
+bob = person(True, False, False,36,82)
+Steve = person(False, False, False,82,36)
+people = [bob,Steve]
+
+
 pygame.init()
 FPS = 30
 air_resistance = 0.97
@@ -8,25 +21,13 @@ dx = 640
 dy = 480
 display = pygame.display.set_mode((dx, dy))
 clock = pygame.time.Clock()
-ox = 36
-oy = 36
-xc = 100
-yc = 0
-s = 35
+s = 5
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     pygame.draw.rect(display, pygame.Color(255,255,255),pygame.Rect(0,0,dx,dy))
-    ox+=xc
-    oy+=yc
-    yc+=9.81/FPS
-    xc = air_resistance * xc
-    yc = air_resistance * yc
-    pygame.draw.circle(display, pygame.Color(255,0,0), [int(ox),int(oy)], s)
-    if ox >= dx-s or ox<= 0+s:
-        xc = xc*-1
-    if oy >= dy-s or oy<= 0+s:
-        yc = yc * -1
+    for person in people:
+        pygame.draw.circle(display, pygame.Color(255,0,0), [int(person.x),int(person.y)], s)
     pygame.display.update()
     clock.tick(FPS)
