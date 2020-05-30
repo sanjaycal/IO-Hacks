502145ee9632663f947b510f957206122c2f4020
Cole Hilderman <57844324+eeloc10@users.noreply.github.com>
2020-05-30 14:39:42 -0600

    Changed infected vulnerable colour
    When vulnerable people are infected they are now pink instead of red

--- main.py
+++ main.py
@@ -124,6 +124,8 @@ while True:
             if person.infecting:
                 pygame.draw.circle(display, pygame.Color(100,0,0), [int(person.x),int(person.y)], spread_distance)
             pygame.draw.circle(display, pygame.Color(255,0,0), [int(person.x),int(person.y)], s)
+            if person.vulnerable == 3:
+                pygame.draw.circle(display, pygame.Color(255,125,255), [int(person.x),int(person.y)], s)
         elif person.immune:
             if person.dead:
                 pygame.draw.circle(display, pygame.Color(0,0,0), [int(person.x),int(person.y)], s)
