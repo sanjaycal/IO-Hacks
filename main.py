2a71ccde4c8db339e5432aa552c60268c1740ffd
Sanjay Jayaram <sanjay.calgary@gmail.com>
2020-05-30 12:06:46 -0600

    shows spread distance

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ dy = 480
 spread_distance = 10
 time_until_spreader = 1440
 time_until_immune = 2880
-chance_of_infection = 90
-store = [random.randint(0,dx),random.randint(0,dy)]
+chance_of_infection = 50
+store = [[random.randint(0,dx),random.randint(0,dy)],[random.randint(0,dx),random.randint(0,dy)]]
 time = 0
 time_needed_in_store = 60
 chance_of_death = 5
 s = 5
+
 class person():
-    def __init__(self, infected, dead, immune,x,y,store_start):
+    def __init__(self, infected, dead, immune,x,y,store_start,which_store):
         self.infected = infected
         self.count = 0
         self.dead = dead
@@ -28,9 +29,10 @@ class person():
         self.y = y
         self.home = [x,y]
         self.infecting = False
-        self.distance_to_store = math.sqrt((store[0]-self.home[0])*(store[0]-self.home[0])+(store[1]-self.home[1])*(store[1]-self.home[1]))
+        self.distance_to_store = math.sqrt((store[which_store][0]-self.home[0])*(store[which_store][0]-self.home[0])+(store[which_store][1]-self.home[1])*(store[which_store][1]-self.home[1]))
         self.store_start = store_start-self.distance_to_store
         self.store_end = store_start+time_needed_in_store
+        self.store = which_store
     def infect(self,other_person):
         if random.randint(0,100)<chance_of_infection:
             other_person.infected = True
@@ -51,19 +53,19 @@ class person():
             self.x = (goal[0]-self.x)/(d+1)+self.x
             self.y = (goal[1]-self.y)/(d+1)+self.y
 
-def generate_people(num,num_infected):
+def generate_people(num,num_infected, store_ditrobution):
     people = []
     for i in range(num):
-        if i < num_infected:
-            a = person(True,False,False, random.randint(0,dx), random.randint(0,dy), random.randint(120,1440))
+        if i < store_ditrobution[0]:
+            a = person(i < num_infected,False,False, random.randint(0,dx/10)*10, random.randint(0,dy/10)*10, random.randint(120,1440),0)
             people.append(a)
-        if i >= num_infected:
-            a = person(False,False,False, random.randint(0,dx), random.randint(0,dy), random.randint(120,1440))
+        if i >= store_ditrobution[0]:
+            a = person(i < num_infected,False,False, random.randint(0,dx/10)*10, random.randint(0,dy/10)*10, random.randint(120,1440),1)
             people.append(a)
     return people
 
 
-people = generate_people(100,2)
+people = generate_people(100,2,[30])
 
 
 pygame.init()
@@ -82,6 +84,8 @@ while True:
     pygame.draw.rect(display, pygame.Color(255,255,255),pygame.Rect(0,0,int(dx),int(dy)))
     for person in people:
         if person.infected:
+            if person.infecting:
+                pygame.draw.circle(display, pygame.Color(100,0,0), [int(person.x),int(person.y)], spread_distance)
             pygame.draw.circle(display, pygame.Color(255,0,0), [int(person.x),int(person.y)], s)
         elif person.immune:
             if person.dead:
@@ -104,9 +108,10 @@ while True:
                     if (operson.x-person.x)*(operson.x-person.x)+(operson.y-person.y)*(operson.y-person.y)<spread_distance*spread_distance:
                         operson.infect(person)
         if time > person.store_start and time < person.store_end:
-            person.move(store)
+            person.move(store[person.store])
         else:
             person.move(person.home)
-    pygame.draw.rect(display,pygame.Color(0,100,100), pygame.Rect(store[0]-5,store[1]-5,10,10))
+    pygame.draw.rect(display,pygame.Color(0,100,100), pygame.Rect(store[0][0]-5,store[0][1]-5,10,10))
+    pygame.draw.rect(display,pygame.Color(0,100,100), pygame.Rect(store[1][0]-5,store[1][1]-5,10,10))
     pygame.display.update()
     clock.tick(FPS)
